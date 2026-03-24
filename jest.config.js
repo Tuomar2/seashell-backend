e806a877272130b28ac3513f62e5b1d23718f814
tuomar2 <tuomas.o.w@gmail.com>
2026-03-24 21:17:42 +0200

    test coverage

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,20 @@ const tsJestTransformCfg = createDefaultPreset().transform;
 /** @type {import("jest").Config} **/
 module.exports = {
   testEnvironment: "node",
+
   transform: {
     ...tsJestTransformCfg,
   },
+
+  // 👇 ADD THESE
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageReporters: ["lcov", "text"],
+
+  // Optional but VERY useful
+  collectCoverageFrom: [
+    "src/**/*.{ts,js}",
+    "!src/**/*.d.ts",
+    "!src/**/index.ts",
+  ],
 };
