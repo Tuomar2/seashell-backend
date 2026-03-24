8011e205c4b8135973cb27fd37336819393e81c3
tuomar2 <tuomas.o.w@gmail.com>
2026-03-24 21:57:59 +0200

    fixed jest config

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,3 @@
-const { createDefaultPreset } = require("ts-jest");
-
-const tsJestTransformCfg = createDefaultPreset().transform;
-
-/** @type {import("jest").Config} **/
 module.exports = {
   testEnvironment: "node",
 
@@ -10,15 +5,16 @@ module.exports = {
     ...tsJestTransformCfg,
   },
 
-  // 👇 ADD THESE
   collectCoverage: true,
   coverageDirectory: "coverage",
   coverageReporters: ["lcov", "text"],
 
-  // Optional but VERY useful
   collectCoverageFrom: [
     "src/**/*.{ts,js}",
     "!src/**/*.d.ts",
     "!src/**/index.ts",
   ],
+
+  // 👇 ADD THIS
+  testPathIgnorePatterns: ["/node_modules/", "/dist/"],
 };
